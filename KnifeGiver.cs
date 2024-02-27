26b4a6bb9f2e2188f7eb9ddf3f548bd6cd9e23b1
jithatsonei <me@ji.wtf>
2024-02-27 02:07:53 +0000

    init

---
+++ KnifeGiver.cs
@@ -0,0 +1,34 @@
+using System;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace KnifeGiver;
+[MinimumApiVersion(96)]
+
+public class KnifeGiver : BasePlugin
+{
+    public override string ModuleName => "Knife Giver";
+    public override string ModuleAuthor => "ji";
+    public override string ModuleDescription => "Ensures players in custom gamemodes spawn with a knife.";
+    public override string ModuleVersion => "build1";
+
+    // This happens when the plugin is loaded
+    public override void Load(bool hotReload)
+    {
+        // Registers and hooks in to the game events we intend to use
+        RegisterEventHandler<EventPlayerSpawn>(Event_PlayerSpawn, HookMode.Post);
+    }
+
+    private HookResult Event_PlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+        
+        if(!player.IsValid || !player.PlayerPawn.IsValid) return HookResult.Continue;       
+        player.GiveNamedItem("weapon_knife"); 
+        return HookResult.Continue;
+    }
+}
