9e528c82926f47b72c8ee3c460028c941ee8609b
jithatsonei <me@ji.wtf>
2024-02-28 17:18:42 -0600

    Update KnifeGiver.cs

--- KnifeGiver.cs
+++ KnifeGiver.cs
@@ -1,4 +1,3 @@
-using System;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
@@ -7,60 +6,67 @@ using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API.Modules.Cvars;
 using CounterStrikeSharp.API.Modules.Utils;
 
-namespace KnifeGiver;
-[MinimumApiVersion(175)]
-
-public class KnifeGiver : BasePlugin
+namespace KnifeGiver
 {
-    public override string ModuleName => "WeaponsGiver";
-    public override string ModuleAuthor => "ji";
-    public override string ModuleDescription => "Ensures players in custom gamemodes spawn with starting weapons.";
-    public override string ModuleVersion => "build3";
-
-    public override void Load(bool hotReload)
+    [MinimumApiVersion(175)]
+    public class KnifeGiver : BasePlugin
     {
-        RegisterEventHandler<EventPlayerSpawn>(Event_PlayerSpawn, HookMode.Post);
-        RegisterEventHandler<EventRoundPrestart>(Event_RoundPrestart, HookMode.Pre);
-    }
+        private string tPrimary;
+        private string tSecondary;
+        private string tMelee;
+        private string ctPrimary;
+        private string ctSecondary;
+        private string ctMelee;
 
-    public void GetVars()
-    {
-        var tPrimary = ConVar.Find("mp_t_default_primary").StringValue;
-        var tSecondary = ConVar.Find("mp_t_default_secondary").StringValue;
-        var tMelee = ConVar.Find("mp_t_default_melee").StringValue;
+        public override string ModuleName => "WeaponsGiver";
+        public override string ModuleAuthor => "ji";
+        public override string ModuleDescription => "Ensures players in custom gamemodes spawn with starting weapons.";
+        public override string ModuleVersion => "build3";
 
-        var ctPrimary = ConVar.Find("mp_ct_default_primary").StringValue;
-        var ctSecondary = ConVar.Find("mp_ct_default_secondary").StringValue;
-        var ctMelee = ConVar.Find("mp_ct_default_melee").StringValue;
-    }
-
-    private HookResult Event_RoundPrestart(EventRoundPrestart @event, GameEventInfo info)
-    {
-        GetVars();
-        return HookResult.Continue;
-    }
-
-    private HookResult Event_PlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
-    {
-        var player = @event.Userid;
-        
-        if(!player.IsValid || !player.PlayerPawn.IsValid) return HookResult.Continue;       
-
-        switch((CsTeam)player.TeamNum)
+        public override void Load(bool hotReload)
         {
-            case CsTeam.Terrorist:
-                if(!String.IsNullOrEmpty(tPrimary)) player.GiveNamedItem(tPrimary);
-                if(!String.IsNullOrEmpty(tSecondary)) player.GiveNamedItem(tSecondary);
-                if(!String.IsNullOrEmpty(tMelee)) player.GiveNamedItem(tMelee);
-                break;
-            
-            case CsTeam.CounterTerrorist:
-                if(!String.IsNullOrEmpty(ctPrimary)) player.GiveNamedItem(ctPrimary);
-                if(!String.IsNullOrEmpty(ctSecondary)) player.GiveNamedItem(ctSecondary);
-                if(!String.IsNullOrEmpty(ctMelee)) player.GiveNamedItem(ctMelee);
-                break;
+            RegisterEventHandler<EventPlayerSpawn>(Event_PlayerSpawn, HookMode.Post);
+            RegisterEventHandler<EventRoundPrestart>(Event_RoundPrestart, HookMode.Pre);
+            GetVars();
         }
 
-        return HookResult.Continue;
+        public void GetVars()
+        {
+            tPrimary = ConVar.Find("mp_t_default_primary").StringValue;
+            tSecondary = ConVar.Find("mp_t_default_secondary").StringValue;
+            tMelee = ConVar.Find("mp_t_default_melee").StringValue;
+            ctPrimary = ConVar.Find("mp_ct_default_primary").StringValue;
+            ctSecondary = ConVar.Find("mp_ct_default_secondary").StringValue;
+            ctMelee = ConVar.Find("mp_ct_default_melee").StringValue;
+        }
+
+        private HookResult Event_RoundPrestart(EventRoundPrestart @event, GameEventInfo info)
+        {
+            return HookResult.Continue;
+        }
+
+        private HookResult Event_PlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+        {
+            var player = @event.Userid;
+            
+            if(!player.IsValid || !player.PlayerPawn.IsValid) return HookResult.Continue;       
+
+            switch((CsTeam)player.TeamNum)
+            {
+                case CsTeam.Terrorist:
+                    if(!String.IsNullOrEmpty(tPrimary)) player.GiveNamedItem(tPrimary);
+                    if(!String.IsNullOrEmpty(tSecondary)) player.GiveNamedItem(tSecondary);
+                    if(!String.IsNullOrEmpty(tMelee)) player.GiveNamedItem(tMelee);
+                    break;
+                
+                case CsTeam.CounterTerrorist:
+                    if(!String.IsNullOrEmpty(ctPrimary)) player.GiveNamedItem(ctPrimary);
+                    if(!String.IsNullOrEmpty(ctSecondary)) player.GiveNamedItem(ctSecondary);
+                    if(!String.IsNullOrEmpty(ctMelee)) player.GiveNamedItem(ctMelee);
+                    break;
+            }
+
+            return HookResult.Continue;
+        }
     }
 }
